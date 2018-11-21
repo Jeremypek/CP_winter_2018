7792cbcef3c13b043104293e6153d5d13b0364c7
Jeremypek <jeremypek93@hotmail.com>
2018-11-21 10:36:31 +0100

    Testing

--- src/pl/waw/sgh/FileSize.java
+++ src/pl/waw/sgh/FileSize.java
@@ -3,7 +3,7 @@ package pl.waw.sgh;
 import java.io.File;
 
 public class FileSize {
-
+/*
     public static void main(String[] args) {
         File newFile = new File("c:/temp");
 
@@ -15,4 +15,29 @@ public class FileSize {
             }
         }
     }
+}
+
+
+public static long CheckDirectorySize(File newFile) { //newFile: "C:\temp"
+    long abc=0;
+    if (newFile.isDirectory()) {
+        File[] files = newFile.listFiles();
+        System.out.println("W folderze " + newFile.getName()+" sa " + files.length + " plik");
+        for (File f : files) {
+            if(f.isFile()) {
+                System.out.println("Plik " + f.getName() + " rozmiar:" + f.length());
+                abc += f.length();
+            } else if (f.isDirectory()) {
+                long subfolderSize = CheckDirectorySize(f);
+                System.out.println("Podfolder " + f.getName() + " size: " +subfolderSize);
+                abc += subfolderSize;
+            }
+        }
+
+    }
+    return abc;
+}
+public
+
+*/
 }
